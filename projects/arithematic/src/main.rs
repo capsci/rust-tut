eaa57a25afa83823c11acf02f97c7593bc045004
ksomani <ksomani@mathworks.com>
2025-05-29 14:20:52 -0400

    data types in Rust

---
+++ projects/arithematic/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("integer addition {}", 5 + 10);
+    println!("floating addition {}", 1.56 + 4.22);
+    println!("substraction {}", 95.5 - 4.3);
+    println!("product {}", 4*10);
+    println!("integer division truncates towards 0 to lowest value {}", -5/3);
+    println!("floating division gives quotient {}", 56.7/32.2 );
+    println!("remainder {}", 43%5);
+}
--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,6 +1,45 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("x is {x}");
     x = 6;
     println!("updated x is {x}");
+
+    // shadowing variable is allowed; Why? doesn't it defeat the purpose of making variable
+    // immutable ?
+    let y = 10;
+    println!("y is {y}");
+    let y = 6;
+    println!("updated y is {y}");
+    let y = "i'm shadowed";
+    println!("newly updated y is {y}");
+
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+    println!("{} {} {}", c, z, heart_eyed_cat);
+
+    // tuples
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup; // "destructuring a tuple"
+    println!("The value of y is: {y}");
+    println!("The value of tup.1(accessed via index) is: {}", tup.1);
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Array is {:?}", a); // need `:?` for printing array
+    let a = [3; 5];
+    println!("Array is {:?}", a);
+
+    println!("Access element at an index");
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Enter index to fetch");
+    let mut index = String::new();
+    std::io::stdin()
+        .read_line(&mut index)
+        .expect("failed to read line");
+    let index:usize = index
+        .trim()
+        .parse()
+        .expect("numeric value expected");
+    let element = a[index]; // panics if index is "out of bounds"
+    println!("Element at index '{index}' is: {element}")
 }
